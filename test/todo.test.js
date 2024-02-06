77623168bdae8f4b7d58691e64dbfc675c9b12b8
HasanOmar1 <r3domar1@gmail.com>
2024-02-06 16:39:17 +0200

    added

--- test/todo.test.js
+++ test/todo.test.js
@@ -8,15 +8,18 @@ const expect = chai.expect;
 
 // i can do describe.only || it.only , to test only one thing
 
-describe.only("Todo Get request", () => {
+describe("Todo query requests", () => {
   describe("testing the GET request", () => {
-    it("testing the GET request", (done) => {
+    it("Should check if I have data", (done) => {
       app.get("/api/v1/todos").end((err, res) => {
         // console.log(res.body);
         if (err) {
           done(err);
         }
-        expect(res.body).to.be.an("array");
+        expect(res.body).to.be.an(
+          "array",
+          "This should always give me an array"
+        );
         done();
       });
     });
@@ -24,55 +27,56 @@ describe.only("Todo Get request", () => {
     it("Checks if we have no todos", (done) => {
       app.get("/api/v1/todos").end((err, res) => {
         expect(res.status).to.equal(200);
-        // expect(res.body).length(0);
+        expect(res.body).length(0);
+        expect(res.body).to.be.empty;
         done();
       });
     });
+
+    // describe("Todo POST request", () => {
+    //   it("Should add a new todo", (done) => {
+    //     app
+    //       .post("/api/v1/todos/add")
+    //       .send({ todo: "kola" })
+    //       .end((err, res) => {
+    //         if (err) {
+    //           return done(err);
+    //         }
+    //         // console.log(res.body);
+    //         expect(res.status).to.equal(201);
+    //         expect(res.body.todo).to.equal("kola");
+    //         done();
+    //       });
+    //   });
+    // });
+
+    // describe("Todo update request", () => {
+    //   it("Should update todo by id", (done) => {
+    //     app
+    //       .put("/api/v1/todos/update/65c224cf5fb1b90e0545a938")
+    //       .send({ todo: "weeew" })
+    //       .end((err, res) => {
+    //         if (err) {
+    //           done(err);
+    //         }
+    //         expect(res.status).to.equal(200);
+    //         expect(res.body.todo).to.equal("weeew");
+    //         done();
+    //       });
+    //   });
+    // });
+
+    // describe("Todo DELETE request", () => {
+    //   it("Should delete todo by id", (done) => {
+    //     app.delete("/api/v1/todos/65c2326240bb10d73874222a").end((err, res) => {
+    //       if (err) {
+    //         done(err);
+    //       }
+
+    //       expect(res.status).to.be.equal(200); // if id not found it should return 404
+    //       done();
+    //     });
+    //   });
+    // });
   });
 });
-
-// describe("Todo POST request", () => {
-//   it("Should add a new todo", (done) => {
-//     app
-//       .post("/api/v1/todos/add")
-//       .send({ todo: "wew" })
-//       .end((err, res) => {
-//         if (err) {
-//           return done(err);
-//         }
-//         // console.log(res.body);
-//         expect(res.status).to.equal(201);
-//         expect(res.body.todo).to.equal("wew");
-//         done();
-//       });
-//   });
-// });
-
-describe("Todo update request", () => {
-  it("Should update todo by id", (done) => {
-    app
-      .put("/api/v1/todos/update/65c224cf5fb1b90e0545a938")
-      .send({ todo: "weeew" })
-      .end((err, res) => {
-        if (err) {
-          done(err);
-        }
-        expect(res.status).to.equal(200);
-        expect(res.body.todo).to.equal("weeew");
-        done();
-      });
-  });
-});
-
-// describe("Todo DELETE request", () => {
-//   it("Should delete todo by id", (done) => {
-//     app.delete("/api/v1/todos/65c21f6fc03a9c2c87b7ad90").end((err, res) => {
-//       if (err) {
-//         done(err);
-//       }
-
-//       expect(res.status).to.be.equal(200); // if id not found it should return 404
-//       done();
-//     });
-//   });
-// });
